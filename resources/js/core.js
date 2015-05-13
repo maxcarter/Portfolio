cf18b5a9ed147cfadf236465579b4b333ee2b04a
maxcarter <max4carter@gmail.com>
2015-05-13 00:07:16 -0400

    Navigation animation

--- resources/js/core.js
+++ resources/js/core.js
@@ -56,4 +56,44 @@ $(document).ready(function() {
     }
     checkWidth();
     $(window).resize(checkWidth); 
+
+    /**
+     * NavMenu animations
+     */
+    $(window).bind( 'hashchange', function(e) { 
+        var anchor = document.location.hash;
+            if( anchor === '#home' ) {
+                $('#navmenu').stop().fadeOut( "slow", "linear");
+                $( "#logo-hex" ).fadeOut("slow","linear", function(){
+                    $( "#logo-hex" ).removeAttr( 'style' );
+                });
+            } else {
+                $( "#logo-hex" ).css({"display":"block"});
+                $('#navmenu').stop().fadeIn( "slow", "linear", function(){
+                    $( "#logo-hex" ).animate({
+                        top: "19px"
+                    }, 500, function() {
+                      // Animation complete.
+                    });
+                });
+            }
+    });
+
+    /**
+     * FlowType
+     * Adjusts font size based on screen size
+     */
+    $('p').flowtype({
+        minFont : 12,
+        maxFont : 25
+    });
+
+    /**
+     * Scroll To
+     */
+    $(".scroll").click(function(event){     
+            event.preventDefault();
+            $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+        });
+
 });
