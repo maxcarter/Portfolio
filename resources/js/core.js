e28b499ce68a82e613e2dd7cd8df3902a388c2de
maxcarter <max4carter@gmail.com>
2015-05-15 15:53:27 -0400

    Parallax hexagon background

--- resources/js/core.js
+++ resources/js/core.js
@@ -112,6 +112,24 @@ $(document).ready(function() {
             $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
         });
 
+
+    /**
+     * Parallax Space Background
+     */
+    var strength1 = 50;
+    var strength2 = 100;
+    var strength3 = 200;
+    $("html").mousemove(function(e){
+        var pageX = e.pageX - ($(window).width() / 2);
+        var pageY = e.pageY - ($(window).height() / 2);
+        var newvalueX = 1* pageX * -1;
+        var newvalueY = 1* pageY * -1;
+        $('#background').css("background-position", (strength1 / $(window).width() * pageX * -1)+"px "+(strength1  / $(window).height() * pageY * -1)+"px");
+        $('#middleground').css("background-position", (strength2 / $(window).width() * pageX * -1)+"px "+(strength2  / $(window).height() * pageY * -1)+"px");
+        $('#foreground').css("background-position", (strength3 / $(window).width() * pageX * -1)+"px "+(strength3  / $(window).height() * pageY * -1)+"px");
+    });
+
+
     /**
      * Contact Form Submit
      * Makes post request to formSubmit.php and processes response.
