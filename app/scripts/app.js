996a67b48d56f520532b5d4d1824a2de2b6e63e3
maxcarter <max4carter@gmail.com>
2016-05-07 12:52:34 -0600

    Removed unused controller

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -28,11 +28,6 @@ angular
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
       .otherwise({
         redirectTo: '/'
       });
