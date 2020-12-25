bb4fd85ad4da583c472140af9fc6e2a2bb1983bd
Maxwell Carter <max4carter@gmail.com>
2020-12-24 16:08:21 -0800

    Fixing flowtype directive

--- app/scripts/directives/flowtype.js
+++ app/scripts/directives/flowtype.js
@@ -7,10 +7,21 @@
  * # flowtype
  */
 angular.module('portfolioApp')
-  .directive('flowtype', function () {
+  .directive('flowtype', ['$window', function ($window) {
     return {
       restrict: 'A',
       link: function postLink(scope, element, attrs) {
+
+        // This function is based on Flowtype.js
+        var adjustSize = function(el, settings) {
+           var $el = $(el),
+              elw = $el.width(),
+              width = elw > settings.maximum ? settings.maximum : elw < settings.minimum ? settings.minimum : elw,
+              fontBase = width / settings.fontRatio,
+              fontSize = fontBase > settings.maxFont ? settings.maxFont : fontBase < settings.minFont ? settings.minFont : fontBase;
+           $el.css('font-size', fontSize + 'px');
+        };
+
           var opt = {};
           opt.maximum = attrs.maximum || 9999;
           opt.minimum = attrs.minimum || 1;
@@ -18,7 +29,11 @@ angular.module('portfolioApp')
           opt.maxFont = attrs.maxFont || 9999;
           opt.fontRatio = attrs.fontRatio || 35;
           opt.lineRatio = attrs.lineRatio || 1.45;
-          element.flowtype(opt);
+
+          angular.element($window).on('resize', function(){
+            adjustSize(element, opt);
+          });
+          adjustSize(element, opt);
       }
     };
-  });
+  }]);
